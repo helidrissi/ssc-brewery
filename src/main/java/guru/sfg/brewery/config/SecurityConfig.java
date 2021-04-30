c173f18a8b5dc109b88781c4c063824322e58e8d
helidrissi <hamza.elidrissi@mistral.fr>
2021-04-30 10:22:52 +0200

    In memory authentication provider

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,10 +1,16 @@
 package guru.sfg.brewery.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -15,11 +21,42 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.
                 authorizeRequests(authorize -> {
                     authorize.antMatchers("/", "/login", "/resources/**", "/webjars/**").permitAll();
-                    authorize.antMatchers(HttpMethod.GET,"/api/v1/beer/**").permitAll();
-                    authorize.mvcMatchers(HttpMethod.GET,"/api/v1/beerUpc/**").permitAll();
+                    authorize.antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll();
+                    authorize.mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/**").permitAll();
                 }).authorizeRequests()
                 .anyRequest().authenticated()
                 .and().formLogin()
                 .and().httpBasic();
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+        .withUser("spring")
+        .password("{noop}guru")
+                .roles("ADMIN")
+                .and()
+                .withUser("user")
+                .password("{noop}password")
+                .roles("USER");
+    }
+
+    /* @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("spring")
+                .password("guru")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user =  User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+
+        return new InMemoryUserDetailsManager(admin,user);
+    }*/
 }
